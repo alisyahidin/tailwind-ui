a9e157251bf7483338b8a8b3c0d2d4b8d6d5ba7d
diana <diana.tampubolon@sg-dsa.com>
2022-03-17 10:33:28 +0700

    add folder utilities and config

--- packages/core/src/components/tailwind.config.js
+++ packages/core/src/components/tailwind.config.js
@@ -1,4 +1,4 @@
-// const plugin = require('tailwindcss/plugin')
+const plugin = require('tailwindcss/plugin')
 const colors = require('tailwindcss/colors')
 
 module.exports = {
@@ -6,14 +6,33 @@ module.exports = {
     colors: {
       ...colors,
       ...require('../colors')
+    },
+    fontSize: {
+      '10': '0.625rem',
+      '12': '0.75rem',
+      '14': '0.875rem',
+      '16': '1rem',
+      '24': '1.5',
+      '28': '1.75rem'
+    },
+    extend: {
+      lineHeight: {
+        '12': '0.75rem',
+        '14': '0.875rem',
+        '16': '1rem',
+        '18': '1.125rem',
+        '21': '1.313rem',
+        '26': '1.625rem',
+        '30': '1.875rem',
+      }
     }
   },
-  // plugins: [
-  //   plugin(function ({ addBase, addUtilities }) {
-  //     addBase(require('../../dist/base'))
-  //     addUtilities(require('../../dist/utilities'), { variants: ['responsive'] })
-  //     addUtilities(require('../../dist/utilities-unstyled'), { variants: ['responsive'] })
-  //     addUtilities(require('../../dist/utilities-styled'), { variants: ['responsive'] })
-  //   })
-  // ],
+  plugins: [
+    plugin(function({ addBase, addUtilities }) {
+      // addBase(require('../../dist/base'))
+      addUtilities(require('../utilities'), { variants: ['responsive'] })
+      // addUtilities(require('../../dist/utilities-unstyled'), { variants: ['responsive'] })
+      // addUtilities(require('../../dist/utilities-styled'), { variants: ['responsive'] })
+    })
+  ],
 }

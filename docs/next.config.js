75de4748f53de38d2597774bddd5130b0f6cb04e
Ali Syahidin <ali.syahidin@sg-dsa.com>
2022-03-22 14:46:49 +0700

    update core dependencies in docs

--- docs/next.config.js
+++ docs/next.config.js
@@ -7,6 +7,27 @@ module.exports = {
     config.module.rules = [
       ...config.module.rules,
       {
+        test: /\.(js|jsx)$/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            sourceType: 'unambiguous',
+            plugins: [
+              [
+                'babel-plugin-module-resolver',
+                {
+                  alias: {
+                    // all packages in this monorepo
+                    '@alisyahidin/core': '../packages/core/dist',
+                  },
+                  transformFunctions: ['require'],
+                },
+              ],
+            ],
+          },
+        },
+      },
+      {
         test: /\.(tsx|ts|js|jsx)$/,
         include: [resolvedBaseUrl],
         use: defaultLoaders.babel,
--- docs/pages/_app.js
+++ docs/pages/_app.js
@@ -2,8 +2,8 @@ import '../styles/globals.css'
 // import '@alisyahidin/react/themes.css'
 
 // for realtime development
-import '@alisyahidin/core/dist/components/index.css'
-import '@alisyahidin/core/dist/utilities/index.css'
+import '@alisyahidin/core/components/index.css'
+import '@alisyahidin/core/utilities/index.css'
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />

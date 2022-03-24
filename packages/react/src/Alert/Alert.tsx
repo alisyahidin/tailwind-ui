74b8ff77b865d9c9067f37889306cdc2efc3e92c
Ali Syahidin <ali.syahidin@sg-dsa.com>
2022-03-24 14:25:40 +0700

    update version for release test

--- packages/react/src/Alert/Alert.tsx
+++ packages/react/src/Alert/Alert.tsx
@@ -2,7 +2,6 @@ import { FC, HTMLAttributes } from 'react'
 import { ClassNames } from '@emotion/react'
 // @ts-ignore
 import style from '@alisyahidin/core/components/alert'
-import { IcCancel } from '@alisyahidin/icon'
 
 type Variant = 'success' | 'warning' | 'error'
 type Close = 'show' | 'hide'

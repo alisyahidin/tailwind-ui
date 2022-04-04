d867192f312c657f552f5631874eb88b6c61d4ee
diana <diana.tampubolon@sg-dsa.com>
2022-04-04 09:44:28 +0700

    Add doc rules tab in docs page

--- packages/react/src/index.ts
+++ packages/react/src/index.ts
@@ -1,4 +1,5 @@
-export { default as Button } from './Button'
-export { default as Modal } from './Modal'
-export { default as Toggle } from './Toggle'
-export { default as Alert } from './Alert'
+// --- Rules: sort by name asc
+export { default as Alert } from "./Alert";
+export { default as Button } from "./Button";
+export { default as Modal } from "./Modal";
+export { default as Toggle } from "./Toggle";
